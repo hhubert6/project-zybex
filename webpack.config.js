df6d74b1272a60a0c8649e1f1bf97cda018850d2
Hubert <hubertkasprzycki6@gmail.com>
2021-11-16 22:30:16 +0100

    Inital typescript with webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.ts'),
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ],
+  devtool: 'eval-source-map',
+  devServer: {
+    open: true,
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      watch: true,
+    },
+  },
+};
